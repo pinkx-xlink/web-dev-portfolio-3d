411a697a754f331382f427eab06102c6cec032b6
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-25 12:52:42 -0500

    Move the light functionality from HeroExperience.jsx to HeroLights.jsx

--- src/components/HeroModels/HeroLights.jsx
+++ src/components/HeroModels/HeroLights.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-
 const HeroLights = () => {
   return (
-    <div>HeroLights</div>
+    <>
+        <ambientLight intensity={0.2} color="#1a1a40" />
+        <directionalLight position={[5, 5, 5]} intensity={2} />
+    
+    </>
   )
 }
 
