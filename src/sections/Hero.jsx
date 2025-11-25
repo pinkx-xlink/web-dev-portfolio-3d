bd85a080622db4bfcce7ab60a9aca19f14d2958e
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-25 17:12:27 -0500

    Animate the h1 using GSAP

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,25 @@
 import { words } from "../constants/index.js";
 import Button from "../components/Button.jsx";
 import HeroExperience from "../components/HeroModels/HeroExperience.jsx";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Hero = () => {
+    useGSAP(() => {
+        gsap.fromTo('.hero-text h1',
+            {
+                y: 50,
+                opacity: 0,
+            },
+            {
+                y: 0,
+                opacity: 1,
+                stagger: 0.2,
+                duration: 1,
+                ease: 'power2.inOut'
+            }
+        )
+    })
   return (
     <section id="hero" className="relative 
   overflow-hidden">
