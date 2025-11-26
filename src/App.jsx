7b8f77333842ed8e6fcc5698a165bd009c0d9296
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-26 16:24:46 -0500

    Import FeatureCards component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './sections/Hero'
 import ShowcaseSection from './sections/ShowcaseSection'
 import NavBar from './components/NavBar'
 import LogoSection from './components/LogoSection'
+import FeatureCards from './sections/FeatureCards'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Hero />
         <ShowcaseSection />
         <LogoSection />
+        <FeatureCards />
     </>
   )
 }
