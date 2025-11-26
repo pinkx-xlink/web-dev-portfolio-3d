1aabf700411fb1c8e6aef653499d222fa1092c71
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-26 16:25:17 -0500

    Move file

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from './sections/Hero'
 import ShowcaseSection from './sections/ShowcaseSection'
 import NavBar from './components/NavBar'
-import LogoSection from './components/LogoSection'
+import LogoSection from './sections/LogoSection'
 import FeatureCards from './sections/FeatureCards'
 
 const App = () => {
