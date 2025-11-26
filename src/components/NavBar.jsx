7ae2e4e42e33b3e510dff66d13607bbba0546aab
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-26 15:46:04 -0500

    Create the scrolled useState, which will trigger an event if the user has scrolled

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { navLinks } from '../constants/index.js';
 
 const NavBar = () => {
+
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const isScrolled = window.scrollY > 10;
+            setScrolled(true);
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [])
+    
   return (
     <header className="navbar">
         <div className='inner'>
@@ -8,8 +23,17 @@ const NavBar = () => {
                 Shaiyan | Y
             </a>
 
-            <nav className='desktop'>
-
+            <nav className="desktop">
+                <ul>
+                    {navLinks.map(({ link, name }) => (
+                        <li key={name} className="group">
+                            <a href={link}>
+                                <span>{name}</span>
+                                <span className="underline"/>
+                            </a>
+                        </li>
+                    ))}
+                </ul>
             </nav>
 
             <a href='#contact' className='contact-btn group'>
