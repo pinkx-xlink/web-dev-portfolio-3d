77095cfc8e719257db8c67c48cba0bb64b291898
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-26 15:29:18 -0500

    Init the NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <header className="navbar">
+        <div className='inner'>
+            <a className='logo' href="#hero">
+                Shaiyan | Y
+            </a>
+
+            <nav className='desktop'>
+
+            </nav>
+
+            <a href='#contact' className='contact-btn group'>
+                <div className='inner'>
+                    <span>Contact Me</span>
+                </div>
+            </a>
+        </div>
+    </header>
   )
 }
 
