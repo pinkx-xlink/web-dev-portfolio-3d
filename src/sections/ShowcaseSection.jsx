318c0e199a2bf2e75495b466e4f01924c26e7e32
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-26 15:25:01 -0500

    Create the projects array and map over each project to create a staggered fade-in animation

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -11,7 +11,28 @@ const ShowcaseSection = () => {
     const project2Ref = useRef(null);
     const project3Ref = useRef(null);
 
+    
     useGSAP(() => {
+        const projects = [project1Ref.current, project2Ref.current, project3Ref.current];
+        projects.forEach((card, index) => {
+            gsap.fromTo(
+                card,
+                {
+                    y: 50, opacity: 0
+                },
+                {
+                    y: 0,
+                    opacity: 1,
+                    duration: 1,
+                    delay: 0.3 * (index + 1),
+                    scrollTrigger: {
+                        trigger: card,
+                        start: 'top bottom-=100'
+                    }
+                }
+            )
+        })
+
         gsap.fromTo(
             sectionRef.current, 
             { opacity: 0 }, 
