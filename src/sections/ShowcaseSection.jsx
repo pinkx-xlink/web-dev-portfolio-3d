0c0e08055a0befa1e1144adf011db1e5d7a189cd
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-26 15:15:30 -0500

    Import dependencies and add a fade-in animation to the projects using GSAP

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { gsap } from 'gsap/gsap-core';
+import { ScrollTrigger } from 'gsap/all';
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const ShowcaseSection = () => {
+    const sectionRef = useRef(null);
+    const project1Ref = useRef(null);
+    const project2Ref = useRef(null);
+    const project3Ref = useRef(null);
+
+    useGSAP(() => {
+        gsap.fromTo(
+            sectionRef.current, 
+            { opacity: 0 }, 
+            {opacity: 1, duration: 1.5}
+        )
+    }, []);
+
   return (
-    <div id="work" className='app-showcase'>
+    <section id="work" ref={sectionRef} className="app-showcase">
         <div className="w-full">
             <div className="showcaselayout">
                     {/* LEFT  */}
-                    <div className="first-project-wrapper">
+                    <div className="first-project-wrapper" ref={project1Ref}>
                         <div className="image-wrapper">
                             <img src='/images/project1.png' alt="Ryde" />
                         </div>
@@ -17,9 +35,11 @@ const ShowcaseSection = () => {
                             </p>
                         </div>
                     </div>
+
+                    
                     {/* RIGHT  */}
                     <div className='project-list-wrapper overflow-hidden'>
-                        <div className='project'>
+                        <div className="project" ref={project2Ref}>
                             <div className='image-wrapper bg-[#ffefdb]'>
                                 <img src="images/project2.png" alt="Library Management Platform" />
                             </div>
@@ -27,7 +47,7 @@ const ShowcaseSection = () => {
                         </div>
 
 
-                         <div className='project'>
+                         <div className="project" ref={project3Ref}>
                             <div className='image-wrapper bg-[#ffe7eb]'>
                                 <img src="images/project3.png" alt="YC Directory" />
                             </div>
@@ -38,7 +58,7 @@ const ShowcaseSection = () => {
             </div>
         </div>
 
-    </div>
+    </section>
   )
 }
 
