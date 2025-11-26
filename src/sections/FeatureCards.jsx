6bfda1fff85b2b31ace0c52ccff0e4fae458a48d
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-26 16:32:02 -0500

    Map over and create an info card for each Ability (will change text for these cards later)

--- src/sections/FeatureCards.jsx
+++ src/sections/FeatureCards.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { abilities } from '../constants/index.js'
 
 const FeatureCards = () => {
   return (
-    <div>FeatureCards</div>
+    <div className='w-full paddinx-g-lg'>
+        <div className='mx-auto grid-3-col'>
+            {abilities.map(({ imgPath, title, desc }) => (
+                <div key={title} className='card-border rounded-xl p-8 flex 
+            flex-col gap-4'>
+                <div className='size-14 flex items-center justify-center 
+            rounded-full'>
+                  <img src={imgPath} alt={title} />
+                </div>
+                    <h3>{title}</h3>
+                </div>
+            ))}
+        </div>
+    </div>
   )
 }
 
