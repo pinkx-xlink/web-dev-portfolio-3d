3cb4d3aca6c45d07dbdbba2e135ac01fd5af8475
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-26 14:13:12 -0500

    Create the template for the first project (left side of the page's content)

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const ShowcaseSection = () => {
   return (
-    <div>ShowcaseSection</div>
+    <div id="work" className='app-showcase'>
+        <div className="w-full">
+            <div className="showcaselayout">
+                    {/* LEFT  */}
+                    <div className="first-project-wrapper">
+                        <div className="image-wrapper">
+                            <img src='/images/project1.png' alt="Ryde" />
+                        </div>
+                        <div className='text-content'>
+                            <h2>On demand rides made simple w/ a powerful user-friendly app called Ryde </h2>
+                            <p className="text-white-50 md:text-xl">
+                                an app built with ____ for fast user experiences ... talk about obstacles faced while building app, and tech used
+                            </p>
+                        </div>
+                    </div>
+                    {/* RIGHT  */}
+
+
+            </div>
+        </div>
+
+    </div>
   )
 }
 
