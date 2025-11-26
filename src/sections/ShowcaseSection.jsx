85a5198402bf4c92228ef44b6db08028fc98e755
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-26 14:40:37 -0500

    Add the (templates for) the secondary projects

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -18,7 +18,22 @@ const ShowcaseSection = () => {
                         </div>
                     </div>
                     {/* RIGHT  */}
+                    <div className='project-list-wrapper overflow-hidden'>
+                        <div className='project'>
+                            <div className='image-wrapper bg-[#ffefdb]'>
+                                <img src="images/project2.png" alt="Library Management Platform" />
+                            </div>
+                            <h2>Library Management Platform</h2>
+                        </div>
 
+
+                         <div className='project'>
+                            <div className='image-wrapper bg-[#ffe7eb]'>
+                                <img src="images/project3.png" alt="YC Directory" />
+                            </div>
+                            <h2>YC directory - a startup showcase app</h2>
+                        </div>
+                    </div>
 
             </div>
         </div>
