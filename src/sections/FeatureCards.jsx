d6ae8e548e98c9caf16214c6b290c0915cf422e1
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-26 21:55:49 -0500

    Fix typo

--- src/sections/FeatureCards.jsx
+++ src/sections/FeatureCards.jsx
@@ -2,8 +2,8 @@ import { abilities } from '../constants/index.js'
 
 const FeatureCards = () => {
   return (
-    <div className='w-full paddinx-g-lg'>
-        <div className='mx-auto grid-3-col'>
+    <div className='w-full padding-x-lg'>
+        <div className='mx-auto grid-3-cols'>
             {abilities.map(({ imgPath, title, desc }) => (
                 <div key={title} className='card-border rounded-xl p-8 flex 
             flex-col gap-4'>
@@ -11,7 +11,9 @@ const FeatureCards = () => {
             rounded-full'>
                   <img src={imgPath} alt={title} />
                 </div>
-                    <h3>{title}</h3>
+                    <h3 className='text-white text-2xl font-semibold 
+                mt-2'>{title}</h3>
+                <p className='text-white-50 text-lg'>{desc}</p>
                 </div>
             ))}
         </div>
