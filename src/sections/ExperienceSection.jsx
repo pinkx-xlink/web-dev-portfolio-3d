d7565fbc61d43831369a8a1a58ed9ef065f812d3
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-26 22:04:06 -0500

    Import TitleHeader component

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import TitleHeader from '../components/TitleHeader'
 
 const ExperienceSection = () => {
   return (
     <section id="experience" className='w-full md:mt-40 mt-20
   section-padding xl:px-0'>
       <div className='w-full h-full md:px-20 px-5'>
-
+        <TitleHeader title="Professional Work Experience"
+        sub="💼 My Career Overview" />
       </div>
     </section>
   )
