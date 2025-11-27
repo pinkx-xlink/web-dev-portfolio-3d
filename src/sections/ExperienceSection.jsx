76fe844aa076be2cde9887d53387e39315c195d9
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-26 22:10:34 -0500

    Import expCards, and map over each one to create a UI card for each title

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import TitleHeader from '../components/TitleHeader'
-
+import { expCards } from '../constants/index'
 const ExperienceSection = () => {
   return (
     <section id="experience" className='w-full md:mt-40 mt-20
@@ -8,6 +8,16 @@ const ExperienceSection = () => {
       <div className='w-full h-full md:px-20 px-5'>
         <TitleHeader title="Professional Work Experience"
         sub="💼 My Career Overview" />
+
+        <div className='mt-32 relative'>
+          <div className='relative z-50 xl:space-y-32 space-y-10'>
+            {expCards.map((card) => (
+              <div key={card.title} className='exp-card-wrapper'>
+                {card.title}
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   )
