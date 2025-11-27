c392ff567ed462b67c15e0147585bc41e0cacaf3
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-26 22:56:41 -0500

    Render the GlowCards

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import TitleHeader from '../components/TitleHeader'
 import { expCards } from '../constants/index'
+import GlowCard from '../components/GlowCard'
+
 const ExperienceSection = () => {
   return (
     <section id="experience" className='w-full md:mt-40 mt-20
@@ -11,10 +13,10 @@ const ExperienceSection = () => {
 
         <div className='mt-32 relative'>
           <div className='relative z-50 xl:space-y-32 space-y-10'>
-            {expCards.map((card) => (
+            {expCards.map((card, index) => (
               <div key={card.title} className='exp-card-wrapper'>
-                <div className='xl_w-2/6'>
-                  <GlowCard card={card}>
+                <div className='xl:w-2/6'>
+                  <GlowCard card={card} index={index}>
                     <div>
                       <img src={card.imgPath} alt={card.title} />
                     </div>
