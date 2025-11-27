d4ae28f9c3623b9171ea2d2b5af74254f5526873
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-26 22:12:47 -0500

    Replace {card.title} w/ the dynamic GlowCard component

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -13,7 +13,13 @@ const ExperienceSection = () => {
           <div className='relative z-50 xl:space-y-32 space-y-10'>
             {expCards.map((card) => (
               <div key={card.title} className='exp-card-wrapper'>
-                {card.title}
+                <div className='xl_w-2/6'>
+                  <GlowCard card={card}>
+                    <div>
+                      <img src={card.imgPath} alt={card.title} />
+                    </div>
+                  </GlowCard>
+                </div>
               </div>
             ))}
           </div>
