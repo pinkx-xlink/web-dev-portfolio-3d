305c05c71b0ccea3348d21e55bdb5ed4c585a1ae
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-26 22:06:22 -0500

    Create TitleHeader() that takes in a title and a subtitle

--- src/components/TitleHeader.jsx
+++ src/components/TitleHeader.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-const TitleHeader = () => {
+const TitleHeader = ({ title, sub }) => {
   return (
-    <div>TitleHeader</div>
+    <div className='flex flex-col items-center gap-5'>
+        <div className='hero-badge'>
+            <p>{sub}</p>
+        </div>
+        <div className='font-semibold md:text-5xl text-3xl text-center'>
+            {title}
+        </div>
+    </div>
   )
 }
 
