d63997f8c6b87a6655c71c49c798c910b17d4169
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-28 17:16:47 -0500

    Begin rendering the Testimonials section

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import TitleHeader from '../components/TitleHeader.jsx'
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <section id="testimonials" className='flex-center 
+section-padding'>
+        <div className='w-full h-full md:px-10 px-5'>
+            <TitleHeader title="What People Say about Me?"
+            sub="⭐ Client Feedback Highlights"
+            />
+        </div>
+    </section>
   )
 }
 
