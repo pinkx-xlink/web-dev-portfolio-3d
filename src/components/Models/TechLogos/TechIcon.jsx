b63f983bddcc8b5f53326e0dd306c398a4285fe5
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-28 16:29:30 -0500

    Render the tech stack icons

--- src/components/Models/TechLogos/TechIcon.jsx
+++ src/components/Models/TechLogos/TechIcon.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { Environment, Float, useGLTF } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 
-const TechIcon = () => {
+const TechIcon = ({ model }) => {
+
+  const scene = useGLTF(model.modelPath); 
+
   return (
-    <div>TechIcon</div>
+    <Canvas>
+      <ambientLight intensity={0.3} />
+      <directionalLight position={[5, 5, 5]} intensity={1} />
+      
+      <Environment preset='city' />
+
+      <Float speed={5.5} rotationIntensity={0.5} floatIntensity={0.9}>
+        <group scale={model.scale} rotation={model.rotation}>
+          <primitive object={scene.scene} />
+        </group>
+      </Float>
+    </Canvas>
   )
 }
 
