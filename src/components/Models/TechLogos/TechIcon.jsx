52ee12a2adc0706de9c51ae7e518d372b09092f1
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-28 16:35:02 -0500

    Tweak lighting, color, etc. for the tech stack icons

--- src/components/Models/TechLogos/TechIcon.jsx
+++ src/components/Models/TechLogos/TechIcon.jsx
@@ -1,16 +1,31 @@
-import { Environment, Float, useGLTF } from '@react-three/drei'
+import { Environment, Float, OrbitControls, useGLTF } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
+import { useEffect } from 'react';
+import * as THREE from 'three';
 
 const TechIcon = ({ model }) => {
 
   const scene = useGLTF(model.modelPath); 
 
+  useEffect(() => {
+    // Gets the THREE logo and makes it more visible against the black background
+    if(model.name === 'Interactive Developer') {
+      scene.scene.traverse((child) => {
+        if(child.isMesh && child.name === 'Object_5') {
+          child.material = new THREE.MeshStandardMaterial({ color: 'white' })
+        }
+      })
+    }
+  }, [scene])
+
   return (
     <Canvas>
       <ambientLight intensity={0.3} />
       <directionalLight position={[5, 5, 5]} intensity={1} />
-      
+
       <Environment preset='city' />
+
+      <OrbitControls enableZoom={false} />
 
       <Float speed={5.5} rotationIntensity={0.5} floatIntensity={0.9}>
         <group scale={model.scale} rotation={model.rotation}>
