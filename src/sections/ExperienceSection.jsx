99353c347685e0a696218d9c20f2fc88c58bc431
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-28 14:27:15 -0500

    Add experience dates and responsibilities for each job, and fix typo (misplaced </div> closing tag)

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -5,8 +5,9 @@ import GlowCard from '../components/GlowCard'
 
 const ExperienceSection = () => {
   return (
-    <section id="experience" className='w-full md:mt-40 mt-20
-  section-padding xl:px-0'>
+    <section 
+    id="experience" 
+    className='w-full md:mt-40 mt-20 section-padding xl:px-0'>
       <div className='w-full h-full md:px-20 px-5'>
         <TitleHeader title="Professional Work Experience"
         sub="💼 My Career Overview" />
@@ -21,6 +22,41 @@ const ExperienceSection = () => {
                       <img src={card.imgPath} alt={card.title} />
                     </div>
                   </GlowCard>
+                </div>
+
+                <div className='xl:w-4/6'>
+                  <div className='flex items-start'>
+                    <div className='timeline-wrapper'>
+                      <div className='timeline' />
+                      <div className='gradient-line w-1 h-full' />
+                    </div>
+
+                      <div className='expText flex xl:gap-20 md:gap-10 gap-5 
+                    relative z-20'>
+                        <div className='timeline-logo'>
+                          <img src={card.logoPath} alt="logo" />
+                        </div>
+
+                        <div>
+                          <h1 className="font-semibold text-3xl">{card.title}</h1>
+                          <p className='my-5 text-white-50'>
+                            🗓️ {card.date}  
+                          </p>
+                          <p className='text-[#839cb5] italic'>
+                            Responsibilities 
+                          </p>
+                          <ul className='list-disc ms-5 mt-5 flex flex-col gap-5
+                        text-white-50'>
+                            {card.responsibilities.map((responsibility) => (
+                              <li kry={responsibility} className='text-lg'>
+                                {responsibility}
+                              </li>
+                            )
+                          )}
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             ))}
