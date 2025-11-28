a831ce6cf79a4e86cc27cf504726744c6dad7868
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-28 15:48:05 -0500

    Begin creating the tech-grid element where all the tech icons will be displayed

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import TitleHeader from '../components/TitleHeader.jsx';
 
 const TechStack = () => {
   return (
-    <div>TechStack</div>
+    <div id="skills" className='flex-center section-padding'>
+        <div className='w-full h-full md:px-10 px-5'>
+            <TitleHeader
+              title="My Preferred Tech Stack"
+              sub="🤝 The Skills I Bring to the Table"
+            />
+
+            <div className='tech-grid'>
+                {techStackIcons.map((icon) => (
+                    <div key={icon.name} className='card-border tech-card 
+                overflow-hidden group xl:rounded-full rounded-lg'>
+                      <div className='tech-card-animated-bg' />
+                      <div className='tech-card-content'>
+                        <div className='tech-icon-wrapper'>
+                            
+                        </div>
+                      </div>
+                    </div>
+                ))}
+            </div>
+
+        </div>
+    </div>
   )
 }
 
