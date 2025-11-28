55bf32d043dd3d90d0d647d337c669116cab9830
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-28 17:20:50 -0500

    Import the example/placeholder testimonials

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import TitleHeader from '../components/TitleHeader.jsx'
+import { testimonials } from '../constants/index.js'
+
 const Testimonials = () => {
   return (
     <section id="testimonials" className='flex-center 
 section-padding'>
         <div className='w-full h-full md:px-10 px-5'>
             <TitleHeader title="What People Say about Me?"
-            sub="⭐ Client Feedback Highlights"
+                sub="⭐ Client Feedback Highlights"
             />
+
+            <div className='lg:columns-3 md:columns-2 columns-1 mt-16'>
+                {testimonials.map((testimonial) => (
+                    <p>{testimonial.review}</p>
+                ))}
+            </div>
         </div>
     </section>
   )
