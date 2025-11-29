9a375fc4b48a6c021179b535fe578b11579be19d
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-28 20:59:49 -0500

    Add images for the (not added yet) social links

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import { socialImgs } from '../constants/index.js'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='footer'>
+        <div className='footer-container'>
+            <div className='flex flex-col justify-center'>
+                <a href="/">Visit my blog</a>
+            </div>
+
+            <div className='socials'>
+                {socialImgs.map((img) => (
+                    <a>
+                        <img src={img.imgPath} />
+                    </a>
+                ))}
+            </div>
+        </div>
+    </footer>
   )
 }
 
