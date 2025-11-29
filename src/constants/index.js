63a5d3871a2bd3128bdc4e5693d61b89bf8c5f3e
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-29 01:32:42 -0500

    Replace work experience titles and dates

--- src/constants/index.js
+++ src/constants/index.js
@@ -156,8 +156,8 @@ const expCards = [
     review: "Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
     imgPath: "/images/exp1.png",
     logoPath: "/images/logo1.png",
-    title: "Frontend Developer",
-    date: "January 2023 - Present",
+    title: "IT Programmer Analyst @ Zimmer Biomet",
+    date: "May 2025 - August 2025",
     responsibilities: [
       "Developed and maintained user-facing features for the Hostinger website.",
       "Collaborated closely with UI/UX designers to ensure seamless user experiences.",
@@ -168,8 +168,8 @@ const expCards = [
     review: "Adrian’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
     imgPath: "/images/exp2.png",
     logoPath: "/images/logo2.png",
-    title: "Full Stack Developer",
-    date: "June 2020 - December 2023",
+    title: "Administrative Assistant @ Archway Marketing Services",
+    date: "December 2020 - March 2025",
     responsibilities: [
       "Led the development of Docker's web applications, focusing on scalability.",
       "Worked with backend engineers to integrate APIs seamlessly with the frontend.",
@@ -180,8 +180,8 @@ const expCards = [
     review: "Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
     imgPath: "/images/exp3.png",
     logoPath: "/images/logo3.png",
-    title: "React Native Developer",
-    date: "March 2019 - May 2020",
+    title: "Sales Associate @ Lowe's",
+    date: "February 2019 - August 2020",
     responsibilities: [
       "Built cross-platform mobile apps using React Native, integrating with Appwrite's backend services.",
       "Improved app performance and user experience through code optimization and testing.",
