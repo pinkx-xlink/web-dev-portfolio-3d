0dc876693069e6914700c6491679b08d3e16b55b
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-29 15:33:08 -0500

    Update counterItems to reflect my experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -28,12 +28,22 @@ const words = [
   { text: "Code", imgPath: "/images/code.svg" },
 ];
 
+// original values:
+// const counterItems = [
+//   { value: 15, suffix: "+", label: "Years of Experience" },
+//   { value: 200, suffix: "+", label: "Satisfied Clients" },
+//   { value: 108, suffix: "+", label: "Completed Projects" },
+//   { value: 90, suffix: "%", label: "Client Retention Rate" },
+// ];
+
+// MY values:
 const counterItems = [
-  { value: 15, suffix: "+", label: "Years of Experience" },
-  { value: 200, suffix: "+", label: "Satisfied Clients" },
-  { value: 108, suffix: "+", label: "Completed Projects" },
-  { value: 90, suffix: "%", label: "Client Retention Rate" },
+  { value: 7, suffix: "+", label: "Years of Experience" },
+  { value: 3, suffix: "+", label: "Years of Code" },
+  { value: 100, suffix: "+", label: "Completed Projects" },
+  { value: 100, suffix: "%", label: "Excited To See If I'm A Good Fit For Your Team!" },
 ];
+
 
 const logoIconsList = [
   {
