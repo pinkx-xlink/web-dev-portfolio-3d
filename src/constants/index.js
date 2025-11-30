3ab8fe91d55bcc20d11090cba901104ff2a45e29
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-11-29 20:35:34 -0500

    Personalize the logo slideshow to companies I've worked for

--- src/constants/index.js
+++ src/constants/index.js
@@ -45,41 +45,80 @@ const counterItems = [
 ];
 
 
+// ORIGINAL: 
+// const logoIconsList = [
+//   {
+//     imgPath: "/images/logos/company-logo-1.png",
+//   },
+//   {
+//     imgPath: "/images/logos/company-logo-2.png",
+//   },
+//   {
+//     imgPath: "/images/logos/company-logo-3.png",
+//   },
+//   {
+//     imgPath: "/images/logos/company-logo-4.png",
+//   },
+//   {
+//     imgPath: "/images/logos/company-logo-5.png",
+//   },
+//   {
+//     imgPath: "/images/logos/company-logo-6.png",
+//   },
+//   {
+//     imgPath: "/images/logos/company-logo-7.png",
+//   },
+//   {
+//     imgPath: "/images/logos/company-logo-8.png",
+//   },
+//   {
+//     imgPath: "/images/logos/company-logo-9.png",
+//   },
+//   {
+//     imgPath: "/images/logos/company-logo-10.png",
+//   },
+//   {
+//     imgPath: "/images/logos/company-logo-11.png",
+//   },
+// ];
+
+
+// MINE: 
+// personalize to my companies
 const logoIconsList = [
   {
-    imgPath: "/images/logos/company-logo-1.png",
+    imgPath: "/images/archway-logo-resized.png",
   },
   {
-    imgPath: "/images/logos/company-logo-2.png",
+    imgPath: "/images/lowes-logo-resized.png",
   },
   {
-    imgPath: "/images/logos/company-logo-3.png",
+    imgPath: "/images/ssc-logo.png",
   },
   {
-    imgPath: "/images/logos/company-logo-4.png",
+    imgPath: "/images/zimmer-logo-resized.png",
   },
   {
-    imgPath: "/images/logos/company-logo-5.png",
+    imgPath: "/images/anderson-logo.png",
+  },
+    {
+    imgPath: "/images/archway-logo-resized.png",
   },
   {
-    imgPath: "/images/logos/company-logo-6.png",
+    imgPath: "/images/lowes-logo-resized.png",
   },
   {
-    imgPath: "/images/logos/company-logo-7.png",
+    imgPath: "/images/ssc-logo.png",
   },
   {
-    imgPath: "/images/logos/company-logo-8.png",
+    imgPath: "/images/zimmer-logo-resized.png",
   },
   {
-    imgPath: "/images/logos/company-logo-9.png",
+    imgPath: "/images/anderson-logo.png",
   },
-  {
-    imgPath: "/images/logos/company-logo-10.png",
-  },
-  {
-    imgPath: "/images/logos/company-logo-11.png",
-  },
+  
 ];
+
 
 const abilities = [
   {
