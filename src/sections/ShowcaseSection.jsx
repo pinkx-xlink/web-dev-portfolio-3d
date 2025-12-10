32bc5f1de51943b0ec8120e3dd802839e0c69fc8
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-12-09 20:50:23 -0500

    Add another .showcaselayout div, and mirror it (2 projects on the left, one on the right)

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -111,6 +111,77 @@ const ShowcaseSection = () => {
             </div>
           </div>
         </div>
+
+        {/* BOTTOM */}
+        <div className="w-full">
+          <div className="showcaselayout">
+            {/* LEFT  */}
+            <div className="project-list-wrapper overflow-hidden">
+              <div className="project" ref={project2Ref}>
+                <div className="image-wrapper bg-[#ffefdb]">
+                  <img
+                    src="/images/sugar-sprite-crochet.png"
+                    alt="Sugar Sprite Crochet Official Website"
+                  />
+                </div>
+                <h2>
+                  <a href="https://sugarspritecrochet.com/" target="_blank">
+                    Sugar Sprite Crochet Official Website
+                  </a>
+                </h2>
+                <p>
+                  The official website for Sugar Sprite Crochet. Built with
+                  HTML, JavaScript, React, and TailwindCSS.
+                </p>
+              </div>
+
+              <div className="project" ref={project3Ref}>
+                <div className="image-wrapper bg-[#ffe7eb]">
+                  <img src="images/google-clone.png" alt="Google Clone" />
+                </div>
+                <h2>
+                  <a
+                    href="https://shaiyans-google-clone.netlify.app/"
+                    target="_blank"
+                  >
+                    Google Clone
+                  </a>
+                </h2>
+                <p>
+                  A simple but stylish Google clone made to showcase my CSS
+                  skills. Made using HTML, CSS, & JavaScript. Warning: NOT the
+                  real Google! (Any searches will redirect the user to the{" "}
+                  <i>real</i> Google.)
+                </p>
+              </div>
+            </div>
+
+            {/* RIGHT  */}
+            <div className="first-project-wrapper" ref={project1Ref}>
+              <div className="image-wrapper">
+                <img
+                  src="/images/pokemon-game.png"
+                  alt="Pokemone Style Game Project"
+                />
+              </div>
+              <div className="text-content">
+                <h2>
+                  <a
+                    href="https://pokemonstylegame.netlify.app/"
+                    target="_blank"
+                  >
+                    Pokemon Style Game - DEMO
+                  </a>{" "}
+                </h2>
+                <p className="text-white-50 md:text-xl">
+                  Explore the land and battle monsters! This project was my
+                  introduction to GSAP animations, and was built on nostalgia,
+                  JavaScript, and HTML Canvas.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
