cc1b73a993cfea247732107b64f9f0a18dd33a91
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2025-12-30 20:51:51 -0500

    Create file

---
+++ src/components/Card.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  CardActions,
+  Button,
+} from "@mui/material";
+
+/**
+ * InfoCard Component
+ * Displays an image, title, description, and optional action button.
+ */
+const InfoCard = ({ image, title, description, buttonText, onButtonClick }) => {
+  return (
+    <Card
+      sx={{
+        maxWidth: 345,
+        boxShadow: 3,
+        borderRadius: 2,
+        transition: "0.3s",
+        "&:hover": { boxShadow: 6 },
+      }}
+    >
+      {image && (
+        <CardMedia component="img" height="180" image={image} alt={title} />
+      )}
+      <CardContent>
+        <Typography gutterBottom variant="h6" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
+      {buttonText && (
+        <CardActions>
+          <Button size="small" onClick={onButtonClick}>
+            {buttonText}
+          </Button>
+        </CardActions>
+      )}
+    </Card>
+  );
+};
+
+// Prop type validation
+InfoCard.propTypes = {
+  image: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  buttonText: PropTypes.string,
+  onButtonClick: PropTypes.func,
+};
+
+// Default props
+InfoCard.defaultProps = {
+  image: "",
+  buttonText: "",
+  onButtonClick: () => {},
+};
+
+export default InfoCard;
