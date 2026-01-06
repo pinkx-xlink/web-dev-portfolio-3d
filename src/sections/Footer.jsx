8161cd62ac7f9d853caf6d0a885065145eadb6c6
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2026-01-06 12:49:52 -0500

    Add hover effect to links and social icons

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -8,17 +8,35 @@ const Footer = () => {
           className="flex flex-col justify-center md:items-start
         items-center"
         >
-          <a href="https://planet-shaiyan.com/blog/" target="_blank">
+          <a
+            href="https://planet-shaiyan.com/blog/"
+            target="_blank"
+            className="hover:text-pink-400"
+          >
             <i>Visit my blog</i>
           </a>
         </div>
 
-        <div className="socials">
-          {socialImgs.map((img) => (
-            <a className="icon" target="_blank" href={img.url} key={img.url}>
-              <img src={img.imgPath} />
-            </a>
-          ))}
+        <div className="links_container flex flex-col justify-center items-center">
+          <div className="socials">
+            {socialImgs.map((img) => (
+              <a
+                className="icon h-7 w-7 mx-2 hover:bg-pink-400"
+                target="_blank"
+                href={img.url}
+                key={img.url}
+              >
+                <img src={img.imgPath} />
+              </a>
+            ))}
+          </div>
+          <a
+            href="https://github.com/pinkx-xlink/web-dev-portfolio-3d"
+            target="_blank"
+            className="hover:text-pink-400"
+          >
+            Check out the code behind this website
+          </a>
         </div>
 
         <div className="flex flex-col justify-center">
