5dc793fc72ff06bb1bdc3de68e8a665a9ac4a7ea
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2026-01-15 19:14:48 -0500

    Add a video clip of my current project and a description about it

--- src/sections/CurrentProject.jsx
+++ src/sections/CurrentProject.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import myVideo from "./videos/node-mini-message-board.mp4";
 
 const CurrentProject = () => {
-  return <div>CurrentProject</div>;
+  return (
+    <section id="contact" className="flex-center section-padding">
+      <h2>Current Project</h2>
+      <p>
+        I'm currently working on a mini message board app built using NodeJS as
+        a backend and MongoDB for data storage. This project allows users to
+        post messages, view messages from others, and interact in a simple and
+        intuitive interface. Stay tuned for updates as I continue to develop and
+        enhance this application!
+      </p>
+      <video
+        src={myVideo}
+        controls
+        autoPlay
+        muted
+        loop
+        style={{ width: "100%", maxWidth: "600px" }}
+      />
+    </section>
+  );
 };
 
 export default CurrentProject;
