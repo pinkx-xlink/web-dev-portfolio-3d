86e8d2d10f94f64b0fb5b5d96dd2d61a9450f3e8
Shaiyan <132025012+pinkx-xlink@users.noreply.github.com>
2026-06-13 21:33:07 -0400

    Add a cartoon-style profile image; display the image in a different location depending on viewport size

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,6 +4,7 @@ import HeroExperience from "../components/HeroModels/HeroExperience.jsx";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import AnimatedCounter from "../components/AnimatedCounter.jsx";
+import myPicrew from "/images/picrew-me.png";
 
 const Hero = () => {
   useGSAP(() => {
@@ -38,45 +39,65 @@ const Hero = () => {
           className="flex flex-col justify-center
         md:w-full w-screen md:px-20 px-5"
         >
-          <div className="flex flex-col gap-7">
-            <div className="hero-text">
-              <h1>
-                Shaping
-                <span className="slide">
-                  <span className="wrapper">
-                    {words.map((word) => (
-                      <span
-                        key={word.text}
-                        className="flex items-center 
+          <div className="hero__container lg:flex lg:flex-row lg:justify-between lg:items-center">
+            <div className="hero__text-container flex flex-col gap-7">
+              <div className="hero-text">
+                <h1>
+                  Shaping
+                  <span className="slide">
+                    <span className="wrapper">
+                      {words.map((word) => (
+                        <span
+                          key={word.text}
+                          className="flex items-center 
                                 md:gap-3 gap-1 pb-2"
-                      >
-                        <img
-                          src={word.imgPath}
-                          alt={word.text}
-                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1
+                        >
+                          <img
+                            src={word.imgPath}
+                            alt={word.text}
+                            className="xl:size-12 md:size-10 size-7 md:p-2 p-1
                                         rounded-full bg-white-50"
-                        />
-                        <span>{word.text}</span>
-                      </span>
-                    ))}
+                          />
+                          <span>{word.text}</span>
+                        </span>
+                      ))}
+                    </span>
                   </span>
-                </span>
-              </h1>
-              <h1>into Custom Websites</h1>
-              {/* <h1>that Deliver Results</h1> */}
+                </h1>
+                <h1>into Custom Websites</h1>
+                {/* <h1>that Deliver Results</h1> */}
+              </div>
+
+              <p
+                className="text-white-50 md:text-xl relative z-10 
+                pointer-events-none"
+              >
+                Hi, I'm Shaiyan: web developer & small business-owner.
+              </p>
+
+              <div className="profile-image-container flex justify-center lg:justify-end md:hidden">
+                <img
+                  src={myPicrew}
+                  alt="Profile"
+                  className="rounded-tr-[60%] rounded-bl-[60%] object-cover max-sm:h-35"
+                />
+              </div>
+
+              <Button
+                className="md:w-80 md:h-16 w-60 h-12"
+                id="button"
+                text="See my Work"
+              />
             </div>
 
-            <p
-              className="text-white-50 md:text-xl relative z-10 
-                pointer-events-none"
-            >
-              Hi, I'm Shaiyan: web developer & small business-owner.
-            </p>
-            <Button
-              className="md:w-80 md:h-16 w-60 h-12"
-              id="button"
-              text="See my Work"
-            />
+            {/* Profile Image */}
+            <div className="profile-image-container flex justify-center lg:justify-end max-sm:hidden">
+              <img
+                src={myPicrew}
+                alt="Profile"
+                className="lg:w-84 lg:h-84 rounded-tr-[60%] rounded-bl-[60%] object-cover max-sm:h-35"
+              />
+            </div>
           </div>
         </header>
 
